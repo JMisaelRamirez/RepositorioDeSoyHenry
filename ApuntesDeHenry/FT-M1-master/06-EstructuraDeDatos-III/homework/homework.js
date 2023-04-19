637f730df5aec0fd7d6b5a4a26c6e9136afdefd8
JMisaelRamirez <126917741+JMisaelRamirez@users.noreply.github.com>
2023-04-18 23:27:04 -0600

    Se agrego el contenido del modulo 2

--- ApuntesDeHenry/FT-M1-master/06-EstructuraDeDatos-III/homework/homework.js
+++ ApuntesDeHenry/FT-M1-master/06-EstructuraDeDatos-III/homework/homework.js
@@ -20,40 +20,96 @@
 function BinarySearchTree(value) {
   this.value = value;
   this.left = null;
-  this.rigth = null;
+  this.right = null;
 }
 
 BinarySearchTree.prototype.insert = function(nodeValue){
   if(nodeValue > this.value){ // Derecha
-    if(this.rigth !== null){
-      this.rigth = new BinarySearchTree(nodeValue);
-      return
-    }
-    this.rigth.insert(nodeValue);
-
-  } else {
-    if(this.left !== null){
+    if(this.right === null){
+      this.right = new BinarySearchTree(nodeValue);
+    } else this.right.insert(nodeValue);
+    
+  } else { // Izquierda
+    if(this.left === null){ // Si es vacio el espacio
       this.left = new BinarySearchTree(nodeValue);
-      return
-    }
-    this.left.insert(nodeValue);
+    } else this.left.insert(nodeValue); // Se llama la funcion a ese nod
   }
-  
 }
+
 BinarySearchTree.prototype.size = function(){
   // Se hace le recorrido y se define un contador
+  if(this.left === null && this.right === null) return 1;
+  if(this.left !== null && this.right === null) return 1 + this.left.size();
+  if(this.left === null && this.right !== null) return 1 + this.right.size();
+  if(this.left !== null && this.right !== null) return 1 + this.left.size() + this.right.size();
 }
-BinarySearchTree.prototype.contains = function(){
+
+BinarySearchTree.prototype.contains = function(value){
   // Recorrido, hacemos uns instancia, en la condicion principal 
   // definimos si existe ese valor en el nodo actual
+  if(this.value === value) return true;
+  if(value < this.value){
+    if(this.left !== null){
+      return this.left.contains(value);
+    } else return false;
+  }  
+  if(value > this.value){
+    if(this.right !== null){
+      return this.right.contains(value);
+    } else return false;
+  } 
 }
-BinarySearchTree.prototype.depthFirstForEach = function(){
-  // Implementar cada uno de los recorridos. Se utilza un sqitch case para caso
 
+BinarySearchTree.prototype.depthFirstForEach = function(cb, mode){
+  // Implementar cada uno de los recorridos. Se utilza un sqitch case para caso
+  // Pre order -> root left right
+  // In order -> left root right
+  // Post order -> left right root
+  //
+  switch(mode){
+  case 'pre-order':
+    cb(this.value);
+    if(this.left !== null) this.left.depthFirstForEach(cb, mode);
+    if(this.right !== null) this.right.depthFirstForEach(cb, mode);
+    break
+  case 'post-order':
+    if(this.left !== null) this.left.depthFirstForEach(cb, mode);
+    if(this.right !== null) this.right.depthFirstForEach(cb, mode);
+    cb(this.value);
+    break
+  case 'in-order':
+    if(this.left !== null) this.left.depthFirstForEach(cb, mode);
+    cb(this.value);
+    if(this.right !== null) this.right.depthFirstForEach(cb, mode);
+    break
+  default:
+    if(this.left !== null) this.left.depthFirstForEach(cb, mode);
+    cb(this.value);
+    if(this.right !== null) this.right.depthFirstForEach(cb, mode);
+    break
+  }
 }
-BinarySearchTree.prototype.breadthFirstForEach = function(){
+
+BinarySearchTree.prototype.breadthFirstForEach = function(cb, array = []){ // Si no se define el parametro array al momento
+  // de llamar a la funcion por default este parametro sera []
   // Recorre el arbol siguiente la definicion del recorrido. 
+  if(this.left !== null){
+    array.push(this.left);
+  }
+  if(this.right !== null){
+    array.push(this.right);
+  }
+  cb(this.value);
+  if(array.length > 0){
+    array.shift().breadthFirstForEach(cb,array)
+  }
 }
+
+// 
+// Array: [J]
+// Nodo Actual: []
+
+//Callback: cb([A,B,C,D,E,F,H,I,J])
 // No modifiquen nada debajo de esta linea
 // --------------------------------
 
